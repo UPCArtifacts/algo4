02ddd6290628fe3c8f6b4bc79f9ee0cf70a1f252
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-08 14:51:03 +0100

    exo 3 TD 1

--- example_sdm_recursivite/main.c
+++ example_sdm_recursivite/main.c
@@ -4,7 +4,7 @@
 
 void minMaxMoyenne(
     // Parametre d'Entrée
-	int n[], 
+	int n[], int sizeOfArray,
 	// Parametre d'Entrée, et a la fois, sorti
 	int *pmin, int *pmax, float *pmoy
 ){
@@ -14,7 +14,8 @@ void minMaxMoyenne(
 	 
 	 int sum = 0;
 	 
-	for(int i= 0; i< 5; i++ ){
+	for(int i= 0; i< sizeOfArray; i++ ){
+		
 		if(*pmin > n[i]) {
 			*pmin = n[i];
 			}
@@ -24,27 +25,64 @@ void minMaxMoyenne(
 		sum = sum + n[i];
 	}
 	
-	*pmoy = sum / 5; //TODO: create a constant
-	
-	printf("Min %d", *pmin);
-	printf("Max %d", *pmax);
+	*pmoy = sum / sizeOfArray; 
 }
 
 
 int main(int argc, char **argv)
 {
+
 	printf("TD 1\n");
+	// Exo 1 et 2
+	//Tableau statique!!
 	int n[]  = {5,6,7,8,3}; 
+
+	//for (int i=0 ; i< N ; i++) 
+	//	  n[i]=i+1 ; 
 	
 	int resultMax = 0;
 	int resultMin = 0;
 	float resultMoy = 0.0;
 	
 	//Ap
-	minMaxMoyenne(n, &resultMin,  &resultMax, &resultMoy);
+	minMaxMoyenne(n, 5, &resultMin,  &resultMax, &resultMoy);
 	
 	printf("Min %d Max %d Moy %f", resultMin, resultMax, resultMoy);
 	
-	printf("End");
+	// Exo 3:
+	int sizeArray, * p;
+	printf("\nWrite the size of the array:\n");
+    scanf("%d", &sizeArray);
+	printf("Value written %d",sizeArray );
+    //P has the address to the memory where we have sizeArray X sizeof(4) bytes
+	p = (int*)malloc(sizeArray*sizeof(int));
+	
+	//Auxiliary pointer
+	int *pi ;
+	//Copy of addresses
+	pi = p;
+	
+	//Init du tableau dynamique
+	for(int i = 0; i < sizeArray; i++ ){
+		printf("\nWrite  value at place i= %d\n",i);
+		scanf("%d", pi);
+		pi++;
+	}
+	
+	printf("Checking initialization\n");
+	for(int i = 0; i < sizeArray; i++ ){
+		printf("Value at place i= %d is : %d\n", i , *(p+i));
+		
+	}
+	
+	//Reset values
+	resultMax = 0;
+	resultMin = 0;
+	resultMoy = 0.0;
+	
+	minMaxMoyenne(p,  sizeArray, &resultMin,  &resultMax, &resultMoy);
+	printf("Min %d Max %d Moy %f", resultMin, resultMax, resultMoy);
+	
+	printf("\nEnd");
 	return 0;
 }
