6a0b1e019a82b980702b5018c44819648c5b2943
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-08 14:31:42 +0100

    exo 1 et 2 TD 1

--- example_sdm_recursivite/main.c
+++ example_sdm_recursivite/main.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 
+
+
+void minMaxMoyenne(
+    // Parametre d'Entrée
+	int n[], 
+	// Parametre d'Entrée, et a la fois, sorti
+	int *pmin, int *pmax, float *pmoy
+){
+	
+	*pmin = n[0];
+    *pmax = n[0]; 
+	 
+	 int sum = 0;
+	 
+	for(int i= 0; i< 5; i++ ){
+		if(*pmin > n[i]) {
+			*pmin = n[i];
+			}
+		if(*pmax < n[i]) {
+			*pmax = n[i];
+		}
+		sum = sum + n[i];
+	}
+	
+	*pmoy = sum / 5;
+	
+	printf("Min %d", *pmin);
+	printf("Max %d", *pmax);
+}
+
+
 int main(int argc, char **argv)
 {
-	printf("hello world CM2\n");
+	printf("TD 1\n");
+	int n[]  = {5,6,7,8,3}; 
+	
+	int resultMax = 0;
+	int resultMin = 0;
+	float resultMoy = 0.0;
+	
+	//Ap
+	minMaxMoyenne(n, &resultMin,  &resultMax, &resultMoy);
+	
+	printf("Min %d Max %d Moy %f", resultMin, resultMax, resultMoy);
+	
+	printf("End");
 	return 0;
 }
