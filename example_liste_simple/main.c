dbc0db191bd9475e115242453b65080e19999316
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-12 14:23:12 +0100

    exo 2 TD 2

--- example_liste_simple/main.c
+++ example_liste_simple/main.c
@@ -2,8 +2,15 @@
 
 /// Exo 1
 
+struct Client{
+	int nrClient;
+	int identifier;
+	};
+
 struct _cell{
 	int valeur;
+	//Si je veux une liste de Client, je change la valeur de la cellule i.e., du champ 'valeur' 
+	//struct Client valeur;
 	struct _cell * suiv;
 };
 
@@ -18,16 +25,40 @@ typedef struct _cell CELL;
 
 typedef CELL * LIST;
 
+// Exo 2 TD 2. Create cell
+CELL* createCell(int valueDeLaCellule){
+	
+	CELL* newCell;
+	newCell = (CELL*) malloc(sizeof(CELL));
+	//Affectation -init-
+	newCell->valeur = valueDeLaCellule;
+	// return l addresse òu nous avons stocké la nouvelle cellule.
+	return newCell;
+}
 
+// Avec Client: 
+//CELL* createCell(struct Client valueOfClient){
+//....
+///}
 
-
-
-
+//CELL* createCell(int nrClient, int identifier){
+//...
+//}
 
 
 
 int main(int argc, char **argv)
 {
 	printf("hello world TD 2 Liste Simple\n");
+	
+	printf("Exo 2: \n");
+	
+	int valueOfNewElement = 10;
+	CELL * createdCellDix = createCell(valueOfNewElement);
+	
+	printf("La valeur à l'interieur de la cellule est: %d", createdCellDix->valeur);
+	//equivalent:
+	//printf("La valeur à l'interieur de la cellule est: %d", (*createdCellDix).valeur);
+	
 	return 0;
 }
