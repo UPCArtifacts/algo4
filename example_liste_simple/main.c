f221b360bdef4ffb9c88d0d99fb51e01e74eb72d
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-12 14:08:53 +0100

    exo 1 TD 2

--- example_liste_simple/main.c
+++ example_liste_simple/main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+/// Exo 1
+
+struct _cell{
+	int valeur;
+	struct _cell * suiv;
+};
+
+// Redefinition of type: now we call 'CELL' to 'struct _cell'
+typedef struct _cell CELL;
+// Equivalent: 
+//typedef struct _cell{
+//	int valeur;
+//	struct _cell * suiv;
+//} CELL;
+
+
+typedef CELL * LIST;
+
+
+
+
+
+
+
+
+
 int main(int argc, char **argv)
 {
 	printf("hello world TD 2 Liste Simple\n");
