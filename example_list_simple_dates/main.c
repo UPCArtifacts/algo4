27b23f0e689bdc40fd32a51035683d1155cf0bb4
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-15 14:44:05 +0100

    exo 1

--- example_list_simple_dates/main.c
+++ example_list_simple_dates/main.c
@@ -11,9 +11,27 @@ struct _date{
 typedef struct _date DATE;
 typedef DATE * LIST;
 
+DATE* createDate(int jour, int mois, int annee){
+	
+	DATE* newDate;
+	newDate = (DATE*) malloc(sizeof(DATE));
+	//Affectation -init-
+	newDate->jour = jour;
+	newDate->mois = mois;
+	newDate->annee = annee;
+	newDate->suiv = NULL;
+	// return l addresse òu nous avons stocké la nouvelle cellule.
+	return newDate;
+}
+
+
 
 int main(int argc, char **argv)
 {
 	printf("TD 2: Exo 2\n");
+	
+	DATE* dateToday = createDate(15, 1, 2021);
+	
+	printf("date: %d / %d / %d", dateToday->jour, dateToday->mois, dateToday -> annee);
 	return 0;
 }
