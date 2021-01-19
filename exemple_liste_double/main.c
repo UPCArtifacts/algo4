90eb56a8a0410cc8cbeb6f960678c70f2120033c
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-19 13:40:57 +0100

    exo 1 td3

--- exemple_liste_double/main.c
+++ exemple_liste_double/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+// Array of Char
+typedef char Chaine[15];
+
+typedef struct pdt { 
+	Chaine idP; // identifiant Produit 
+   int nbP; // quantite en Stock 
+  float PU; // prix unitaire
+  struct pdt *suiv, *  prec ; // pointeurs vers le suivant et le precedent 
+} Produit, *  Liste;
+
+typedef struct { Liste debut, fin ; } Stock ;
+
 int main(int argc, char **argv)
 {
 	
