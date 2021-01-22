8cd0f6dca230a895c488c489712a0b2fd027ef13
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-22 14:00:21 +0100

    exo 2 TD 3

--- exemple_files_piles/main.c
+++ exemple_files_piles/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+///
+typedef float TAB[20];
+
+typedef  struct {
+	TAB values;	// pile d'éléments
+	int card; // nombre d'éléments dans la pile
+} PILE;
+
+
 int main(int argc, char **argv)
 {
 	printf("TD 3 Exo 2 Files et piles: \n");
+	
+	PILE myPile;
+	myPile.card;
+	myPile.values;
+	
+	
 	return 0;
+	
+	
 }
