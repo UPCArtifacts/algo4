217731a885c7ab4f8d0eaa785569af0badd1ce84
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-22 14:29:28 +0100

    exo 2 TD 3

--- exemple_files_piles/main.c
+++ exemple_files_piles/main.c
@@ -32,6 +32,39 @@ int estVide(PILE p){
 	
 }
 
+//renvoie 1 si p ne contient rien, 0 sinon
+int estVidePointer(PILE * p){
+	//I can use -> because it's a pointer to one struct
+	if(p->card == 0){
+		return 1;
+	}
+	else{
+			return 0;
+			}
+	
+}
+//// ajoute f dans p si f non pleine, renvoie 1 si OK, 0 sinon
+int empiler(PILE * p, float valueToAdd){
+	
+	if (estPleine(*p)) {
+		return 0;
+	}
+else{ 
+	p-> values[p->card]= valueToAdd ;
+     p->card=p->card +1;
+  return 1;
+}
+	
+}
+// renvoie 1 si on ne peut plus rien ajouter dans p, 0 sinon
+int estPleine(PILE p){
+		if(p.card == MAX){
+		return 1;
+	}
+	else{
+			return 0;
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -49,7 +82,22 @@ int main(int argc, char **argv)
 	
 	int isEmpty = estVide(myPile);
 	
-	printf("is Empty? %d", isEmpty);
+	printf("is Empty? %d\n", isEmpty);
+	
+	int isFull = estPleine(myPile);
+	
+	printf("is Full? %d\n", isFull);
+	
+	float firstValue = 0.5;
+	//Put a value 
+	printf("\nAdding a value: \n")
+	int firstIn = empiler(&myPile, firstValue);
+	
+	isEmpty = estVide(myPile);
+	
+	printf("is Empty? %d\n", isEmpty);
+
+	
 	
 	return 0;
 	
