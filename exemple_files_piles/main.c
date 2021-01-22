1af081518a7f3e03a791f7e50e5333951c766af3
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-22 14:09:15 +0100

    exo 2 TD 3

--- exemple_files_piles/main.c
+++ exemple_files_piles/main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
 ///
-typedef float TAB[20];
+#define MAX 20
+#define VALUE_NULL -1
+
+typedef float TAB[MAX];
 
 typedef  struct {
 	TAB values;	// pile d'éléments
 	int card; // nombre d'éléments dans la pile
 } PILE;
+
+//p ; p doit etre vide en sortie de cette fonction.
+void init(PILE *p){
+   
+	p->card = 0;
+	for (int i =0; i< MAX; i++){
+			p->values[i] = VALUE_NULL;
+		}
+	
+}
 
 
 int main(int argc, char **argv)
@@ -16,6 +29,12 @@ int main(int argc, char **argv)
 	PILE myPile;
 	myPile.card;
 	myPile.values;
+	
+	init(&myPile);
+	
+	//Dont use -> because myPile is not a pointer
+	//myPile is a struct so for this reason we use . to access to its values
+	printf("Size of pile: %d \n", myPile.card);
 	
 	
 	return 0;
