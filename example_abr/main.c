91db86f37a03744f27e03f57b78041caeb89a989
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-26 13:43:28 +0100

    td 4

--- example_abr/main.c
+++ example_abr/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+typedef struct _noeud
+{      int val ; // valeur stockee
+       struct _noeud *  fg, * fd; // pointeurs vers les noeuds fils 
+} NOEUD, * ABR ;
+
+
+
+
 int main(int argc, char **argv)
 {
 	printf("TD 4 ABR: \n");
+	
+	ABR a=NULL; // equivalent:  struct _noeud * a = NULL;
+	
 	return 0;
 }
