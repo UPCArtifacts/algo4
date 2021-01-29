c05a15d09af7e1dd3380e0d04c4c0004e8a518d9
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-01-29 14:38:49 +0100

    exo recursivité

--- example_recursivite/main.c
+++ example_recursivite/main.c
@@ -3,22 +3,31 @@
 #define N 5
 typedef int TabEntiers[N];
 
+//The function returns the number of elements (greater than x) 
+//from the table that are at the right of the current position
+int nrValuesGreater(TabEntiers myTab, int x, int currentPosition){
 
-int functionRecursive(int * t, int m, int i) { 
-	if (i >= 0){
-		if (t[i] >m){
-		  return functionRecursive(t, t[i], i-1);
-		}else 
-			return f(t, m, i-1);
-	}else {
-			return m;
+	if(currentPosition  >= 0){
+		
+	if(myTab[currentPosition] > x){
+			return 1 + nrValuesGreater(myTab, x, currentPosition - 1);
+		}
+		else{
+			return 0 + nrValuesGreater(myTab, x, currentPosition - 1);
+			}
+		}
+	else{
+		return 0; 
+		}
+	
 }
-	}
+
 
 int main(int argc, char **argv)
 {
 	printf("Example recursivite: \n");
 	TabEntiers monTab={8, 2, 3, 7, 5};
-printf("%d \n", functionRecursive(monTab, monTab[N−1], N−1));
+	int x = 7;
+	printf("%d \n", nrValuesGreater(monTab, x, N- 1));
 	return 0;
 }
